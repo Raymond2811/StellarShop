cf41cb79b9c7fa7b838684a22f3e55b27bc268ad
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-06-19 00:03:51 -0700

    updated index.js for models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,15 @@
-const User = requier('./User');
+const User = require('./User');
+const Cart = require('./Cart');
+const Category = require('./Category');
+const Order = require('./Order');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 module.exports = {
   User,
-  
+  Cart,
+  Category,
+  Order,
+  Product,
+  Tag
 }
