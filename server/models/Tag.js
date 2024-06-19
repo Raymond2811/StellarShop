0b8c2cc283aa9fbd100a7b9d1bf2ca31561b3dd4
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-06-18 23:50:58 -0700

    added tag model

---
+++ server/models/Tag.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const tagSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  tagGroup: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+const Tag = mongoose.model('Tag', tagSchema);
+
+module.exports = Tag;
