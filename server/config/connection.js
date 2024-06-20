f4bcb077435766c7ee98848bc4e146a245f12c12
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-06-20 00:05:08 -0700

    added auth.js, updated connection.js and server.js, and installed dotenv

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// const mongoOptions = {
-//   useNewURLParser: true,
-//   useUnifiedTopology: true,
-//   serverSelectionTimeoutMS: 30000,
-// }
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/StellarShop');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/StellarShop',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
