ee6993f9e05822953e87896498cff3970924001a
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-06-27 00:04:20 -0700

    added user query

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,33 @@
+const { Cart, Category, Order, Product, Tag, User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+const stripe = require('stripe')('');
+
+const resolvers = {
+  Query: {
+    user: async (_root, args, context) => {
+      if(!context.user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+
+      try {
+        const user = await User.findById(context.user._id)
+          .populate({
+            path: 'orders.products',
+            populate: 'category',
+          })
+          .populate('cart');
+        
+        if (user.orders){
+          user.orders.sort ((a,b) => b.purchaseDate - a.purchaseDate);
+        }
+      
+        return user;
+      } catch( error ) {
+        console.error('Error fetching user:', error);
+        throw new Error('Failed to fetch user data');
+      }
+    } 
+  }
+}
+
+module.exports = resolvers;
