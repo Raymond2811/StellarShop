4b535d5572dfb9f54be5441d0f73c8072903f63d
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-07-21 18:01:34 -0700

    updated seed.js and resolved connection string issue

--- server/config/seed.js
+++ server/config/seed.js
@@ -1,42 +1,42 @@
 const { User, Product, Category } = require('../models');
-const products = require('./seedData');
+const products  = require('./seedData');
 const cleanDB = require('./cleanDB');
 const db = require('./connection');
 
-const seedData = async () => {
-  await cleanDB('Category', 'categories');
-  await cleanDB('Product', 'products');
-  await cleanDB('User', 'users');
-
-  const categories = [
-    {name: 'Computers & Tablets'},
-    {name: 'Video Games'},
-    {name: 'Cell Phones'},
-    {name: 'Cameras & Drones'}
-  ];
-
+async function seedData() {
   try {
+    await cleanDB('Category', 'categories');
+    await cleanDB('Product', 'products');
+    await cleanDB('User', 'users');
 
-    const createdCategories = await Category.insertMany(categories);
+    const categories = await Category.insertMany([
+      { name: 'Computers & Tablets' },
+      { name: 'Video Games' },
+      { name: 'Cell Phones' },
+      { name: 'Cameras & Drones' }
+    ]);
 
-    const categoryMap = createdCategories.reduce((map, category) => {
+    const categoryMap = categories.reduce((map, category) => {
       map[category.name] = category._id;
       return map;
     }, {});
 
-    const productsWithCatgoryIds = products.map(product => ({
+    const productsWithCategoryIds = products.map(product => ({
       ...product,
-      category: categoryMap[product.category],
+      category: categoryMap[product.category]
     }));
+    
+    createdProducts = await Product.insertMany(productsWithCategoryIds);
 
-    await Product.insertMany(productsWithCatgoryIds);
+    for (let product of createdProducts) {
+      await Category.findByIdAndUpdate(product.category, { $push: { products: product._id } });
+    }
 
     console.log('Database seeded successfully');
-  } catch(error){
+  } catch (error) {
     console.error('Error seeding database:', error);
-  } finally{
-    process.exit();
   }
-}; 
+  process.exit();
+};
 
 db.once('open', seedData);
