d0880c92670246f5399e4025c7f45bc32ee2897b
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-08-01 00:00:50 -0700

    added AddUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,22 @@
 import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+mutation AddUser(
+  $firstName: String!, 
+  $email: String!, 
+  $password: String!, 
+  $lastName: String!
+) {
+  addUser(
+    firstName: $firstName, 
+    email: $email, 
+    password: $password, 
+    lastName: $lastName
+  ) {
+    token
+    user {
+      _id
+    }
+  }
+}
+`;
