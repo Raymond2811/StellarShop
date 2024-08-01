a8713be9d2b8cda8a1f9948f0c74e543f4f6cb1c
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-08-01 01:47:02 -0700

    added orders query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,22 @@
 import { gql } from '@apollo/client';
+
+export const ORDERS = gql`
+  query Orders {
+    orders {
+      _id
+      products {
+        product {
+          _id
+          name
+          image
+          description
+          price
+        }
+        purchaseQuantity
+      }
+      purchaseDate
+      status
+      totalAmount
+    }
+  }
+`;
