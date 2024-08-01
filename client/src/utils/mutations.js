ff0f5b359a328257d0113760dbe33eda52e01c96
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-08-01 00:51:08 -0700

    added login mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -63,3 +63,14 @@ mutation Mutation {
   logout
 }
 `;
+
+export const LOGIN = gql`
+mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+    }
+  }
+}
+`;
