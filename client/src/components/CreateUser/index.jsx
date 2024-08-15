18413886db6b1b835fcb9b43cdcff11e561d08e7
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-08-15 02:19:43 -0700

    added user creation, cart sync, and form handling logic

--- client/src/components/CreateUser/index.jsx
+++ client/src/components/CreateUser/index.jsx
@@ -1,6 +1,93 @@
+import { useMutation } from "@apollo/client";
+import Auth from "../../utils/auth";
+import { ADD_USER, ADD_TO_CART } from "../../utils/mutations";
+import { useSelector } from "react-redux";
+import { useState } from 'react';
 
 export default function CreateUser() {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+
+  const [addUser] = useMutation(ADD_USER);
+  const [addToCartMutation] = useMutation(ADD_TO_CART);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+    try {
+    const { data: { addUser: { token } }} = await addUser({
+      variables: {...formData},
+    });
+
+    await Promise.all(cartItems.map(item =>
+      addToCartMutation({
+        variables: {
+          productId: item._id,
+          purchaseQuantity: item.purchaseQuantity,
+        },
+      })
+    ));
+    
+    Auth.login(token);
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    });
+
+    } catch (error) {
+     console.error('Error creating user:', error.message);
+    }
+  }
   return (
-    <h1>Create Account</h1>
+    <div>
+      <h1>Create Account</h1>
+      <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+        required
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+        placeholder="Last Name"
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Create Account</button>
+      </form>
+    </div>
+
   );
 };
