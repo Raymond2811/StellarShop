7bcd42007349147cd0af09471903b4983f65379b
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-08-23 20:17:40 -0700

    intgrated material ui into nav

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -4,19 +4,20 @@ import { loadCategories } from '../../utils/slices/productSlice';
 import { setCurrentCategory } from '../../utils/slices/currentCategorySlice';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink} from 'react-router-dom';
+import { Button, Link, Box } from '@mui/material';
 
 export default function Nav(){
   const dispatch = useDispatch();
   const { categories } = useSelector((state) => state.products)
 
-  const { data, loading, error } = useQuery(QUERY_CATEGORIES);
+  const { data, error } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
     if (data && data.categories){
       dispatch(loadCategories(data.categories));
     }
-  }, [data, dispatch, loading]);
+  }, [data, dispatch]);
 
   const handleCategoryClick = (id) => {
     dispatch(setCurrentCategory(id));
@@ -28,21 +29,21 @@ export default function Nav(){
 
   if (error) return <p>Error: {error.message}</p>
   return (
-    <nav>
-      <ul>
-        {categories.map((category) =>(
-          <li 
-            key={category._id} 
-          >
-            <Link
-             to={`/${convertToUrlFriendly(category.name)}`}
-             onClick={() => handleCategoryClick(category._id)}
-            >
-              {category.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav>  
+      <Box sx={{display: 'flex', gap: 2}}>
+      {categories.map((category) =>(
+        <Link
+          key={category._id}
+          component={RouterLink}
+          to={`/${convertToUrlFriendly(category.name)}`}
+          underline="none"
+          color="inherit"
+          onClick={() => handleCategoryClick(category._id)}
+        >
+         <Button color='inherit'>{category.name}</Button>
+        </Link>
+      ))}
+      </Box>
     </nav>
   );
 };
