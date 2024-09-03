7d0ab512acb8bfb4cde5b80d8235931f2e3f0267
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-09-02 22:14:11 -0700

    added grid layout for login and createUser components

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,12 +1,23 @@
 import CreateUser from "../components/CreateUser";
 import Login from "../components/Login";
+import { Grid, Container } from '@mui/material';
 
 export default function Account() {
   return(
-    <div>
+    <main>
       <h1>Account</h1>
-      <Login/>
-      <CreateUser/>
-    </div>
+      <Container>
+        <Grid container spacing={2}>
+
+          <Grid item sm={12} md={6}>
+            <Login/>
+          </Grid>
+
+          <Grid item sm={12} md={6}>
+            <CreateUser/>
+          </Grid>
+        </Grid>
+      </Container>
+    </main>
   )
 }
